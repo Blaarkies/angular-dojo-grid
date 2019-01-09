e3bef7d494f17f3739c21d98314dfd1c27985055
Pierre Roux <pierre.roux6@gmail.com>
2019-01-09 20:21:39 +0200

    Add animation components

---
+++ src/app/animation/keyframe-animated/keyframe-animated.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-keyframe-animated',
+  templateUrl: './keyframe-animated.component.html',
+  styleUrls: ['./keyframe-animated.component.css']
+})
+export class KeyframeAnimatedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/animation/transistion-animated/transistion-animated.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-transistion-animated',
+  templateUrl: './transistion-animated.component.html',
+  styleUrls: ['./transistion-animated.component.css']
+})
+export class TransistionAnimatedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

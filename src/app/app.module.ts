115b99bde24ccfa3601685780fb0db73de2f1593
Daan Keun <daan.keun@entelect.co.za>
2019-02-13 20:27:43 +0200

    Refactor routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,39 @@ import { FormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
-import { NormalTableComponent } from './grid/normal-table/normal-table.component';
-import { CssGridTableComponent } from './grid/css-grid-table/css-grid-table.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { SearchBarComponent } from './components/search-bar/search-bar.component';
-import { ActionButtonsComponent } from './components/action-buttons/action-buttons.component';
-import { TabsBarComponent } from './components/tabs-bar/tabs-bar.component';
-import { ExtraBarComponent } from './components/extra-bar/extra-bar.component';
-import { ListOfItemsComponent } from './components/list-of-items/list-of-items.component';
-import { BootstrapTableComponent } from './grid/bootstrap-table/bootstrap-table.component';
-import { FlexTableComponent } from './grid/flex-table/flex-table.component';
-import { TransitionAnimatedComponent } from './animation/1-transition-animated/transition-animated.component';
-import { KeyframeAnimatedComponent } from './animation/2-keyframe-animated/keyframe-animated.component';
-import { PracticalButtonComponent } from './animation/3-practical-button/practical-button.component';
+import { NormalTableComponent } from './components/grid/normal-table/normal-table.component';
+import { CssGridTableComponent } from './components/grid/css-grid-table/css-grid-table.component';
+import { NavbarComponent } from './common/navbar/navbar.component';
+import { SearchBarComponent } from './common/search-bar/search-bar.component';
+import { ActionButtonsComponent } from './common/action-buttons/action-buttons.component';
+import { TabsBarComponent } from './common/tabs-bar/tabs-bar.component';
+import { ExtraBarComponent } from './common/extra-bar/extra-bar.component';
+import { ListOfItemsComponent } from './common/list-of-items/list-of-items.component';
+import { BootstrapTableComponent } from './components/grid/bootstrap-table/bootstrap-table.component';
+import { FlexTableComponent } from './components/grid/flex-table/flex-table.component';
+import { TransitionAnimatedComponent } from './components/animations/1-transition-animated/transition-animated.component';
+import { KeyframeAnimatedComponent } from './components/animations/2-keyframe-animated/keyframe-animated.component';
+import { PracticalButtonComponent } from './components/animations/3-practical-button/practical-button.component';
+import { ResponsiveLayoutComponent } from './components/responsive-layout/responsive-layout.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AnimationsComponent } from './components/animations/animations.component';
+import { GridComponent } from './components/grid/grid.component';
+
+const appRoutes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'animations', component: AnimationsComponent},
+    {path: 'grid', component: GridComponent},
+    {path: 'responsive-layout', component: ResponsiveLayoutComponent},
+];
 
 @NgModule({
-    imports: [BrowserModule, FormsModule, FlexLayoutModule],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        FlexLayoutModule,
+        RouterModule.forRoot(appRoutes),
+    ],
     declarations: [
         AppComponent,
         NormalTableComponent,
@@ -35,6 +52,10 @@ import { PracticalButtonComponent } from './animation/3-practical-button/practic
         TransitionAnimatedComponent,
         KeyframeAnimatedComponent,
         PracticalButtonComponent,
+        ResponsiveLayoutComponent,
+        AnimationsComponent,
+        HomeComponent,
+        GridComponent,
     ],
     bootstrap: [AppComponent],
 })
